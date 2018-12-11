e8f428b0ffc464e4fe48042dee78d8168509b35b
cAibDe <you@example.com280206014@qq.com>
2018-12-11 18:04:21 +0800

    Initial Commit

---
+++ SignalPageMove/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SignalPageMove
+//
+//  Created by 张鹏 on 2018/12/11.
+//  Copyright © 2018 c4ibD3. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ SignalPageMove/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SignalPageMove
+//
+//  Created by 张鹏 on 2018/12/11.
+//  Copyright © 2018 c4ibD3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
