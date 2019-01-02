ad534bf09b7ff2d8b8207a923345dffa1b2168c5
cAibDe <you@example.com280206014@qq.com>
2019-01-02 16:47:48 +0800

    优化

--- SignalPageMove/ViewController.m
+++ SignalPageMove/ViewController.m
@@ -8,8 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UICollectionViewDelegate,UICollectionViewDataSource>
 
+@property (nonatomic, strong)UICollectionView *collectionView;
+@property (nonatomic, strong)UICollectionViewFlowLayout *layout;
 @end
 
 @implementation ViewController
@@ -17,7 +19,59 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self configCollectionView];
+}
+- (void)configCollectionView{
+    
+    //1. 设置layout
+    self.layout = [[UICollectionViewFlowLayout alloc]init];
+    self.layout.minimumLineSpacing = 10;
+    self.layout.minimumInteritemSpacing = 10;
+    self.layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    //设置item大小
+    self.layout.itemSize = CGSizeMake(200, 100);
+    //2.collectionVIew
+    self.collectionView = [[UICollectionView alloc]initWithFrame:CGRectMake(0, 100, [UIScreen mainScreen].bounds.size.width, 120) collectionViewLayout:self.layout];
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    //不显示进度条
+    self.collectionView.showsVerticalScrollIndicator = NO;
+    self.collectionView.showsHorizontalScrollIndicator = NO;
+    [self.view addSubview:self.collectionView];
+    //3.注册单元格
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:NSStringFromClass([UICollectionViewCell class])];
+    
+}
+#pragma mark - CollectionView
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+    return 1;
+}
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return 10;
+}
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass([UICollectionViewCell class]) forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor redColor];
+    return cell;
 }
 
-
+- (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset{
+    float pageWidth = 200 + 10;
+    float currentOffset = _collectionView.contentOffset.x;
+    float targetOffset = targetContentOffset->x;
+    float newTargetOffset = 0;
+    if (targetOffset+ 20  > currentOffset) {
+        newTargetOffset = ceilf(currentOffset/pageWidth) * pageWidth;
+    }else{
+        newTargetOffset = floorf(currentOffset/ pageWidth) * pageWidth;
+    }
+    if (newTargetOffset < 0) {
+        newTargetOffset = 0;
+    }else if (ABS(scrollView.contentSize.width - (newTargetOffset + pageWidth))< pageWidth){
+        newTargetOffset = scrollView.contentSize.width - _collectionView.bounds.size.width ;
+    }
+    newTargetOffset = ceilf(newTargetOffset);
+    targetContentOffset->x = currentOffset;
+    [scrollView setContentOffset:CGPointMake(newTargetOffset, 0) animated:YES];
+}
 @end
